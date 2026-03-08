13f8ce1cbf9a5065df018f7fa75029f70c8115bd
Gabriel Silva <gabrieldasilvamonteiro2@gmail.com>
2026-03-08 15:24:22 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/curso/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.curso.workshopmongo.repository;
+
+import com.curso.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+}
