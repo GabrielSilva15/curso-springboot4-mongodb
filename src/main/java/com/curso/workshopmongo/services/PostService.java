0ae39e64f332d4ec05f32266d1379d2c6d64a804
Gabriel Silva <gabrieldasilvamonteiro2@gmail.com>
2026-03-09 12:02:49 -0300

    Obtendo Post por Id

---
+++ src/main/java/com/curso/workshopmongo/services/PostService.java
@@ -0,0 +1,18 @@
+package com.curso.workshopmongo.services;
+
+import com.curso.workshopmongo.domain.Post;
+import com.curso.workshopmongo.repository.PostRepository;
+import com.curso.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        return postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
